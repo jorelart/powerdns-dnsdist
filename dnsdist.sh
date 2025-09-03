63b843e3e2532bc10d61b7a32d3751c36efd8e43
Jorel Aristian <67253819+jorelart@users.noreply.github.com>
2025-09-03 08:31:59 +0700

    Update dnsdist.sh

--- dnsdist.sh
+++ dnsdist.sh
@@ -7,7 +7,7 @@
 echo "deb [signed-by=/etc/apt/keyrings/dnsdist-20-pub.asc] http://repo.powerdns.com/ubuntu noble-dnsdist-20 main" > /etc/apt/sources.list.d/pdns.list
 echo "Package: dnsdist*
 Pin: origin repo.powerdns.com
-Pin-Priority: 600" > /etc/apt/preferences.d/dnsdist-18
+Pin-Priority: 600" > /etc/apt/preferences.d/dnsdist-20
 sudo install -d /etc/apt/keyrings; curl https://repo.powerdns.com/FD380FBB-pub.asc | sudo tee /etc/apt/keyrings/dnsdist-20-pub.asc &&
 sudo apt update -y
 
